772ecbeb82e990a573d248612ebc168051044703
Andresanegas <naslajosua@gmail.com>
2024-11-28 20:58:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,32 +17,7 @@ if st.button("Enviar"):
     st.write(f"Novedad: {novedad}")
     st.write(f"Detalles: {detalles}")
 
-import streamlit as st
-import pandas as pd
 
-st.title(" Yes Bpo")
-
-# Crear un DataFrame vacío para almacenar los datos
-data = pd.DataFrame(columns=['Fecha', 'Nombre', 'Novedad', 'Detalles'])
-
-# Campos del formulario
-fecha = st.date_input("Fecha")
-nombre = st.text_input("Nombre")
-novedad = st.selectbox("Novedad", ["Llegada Tarde", 'Incapacidad'])
-detalles = st.text_area("Detalles")
-
-# Botón para enviar los datos
-if st.button("Enviar"):
-    # Crear un nuevo DataFrame con los datos ingresados
-    new_data = pd.DataFrame({'Fecha': [fecha],
-                             'Nombre': [nombre],
-                             'Novedad': [novedad],
-                             'Detalles': [detalles]})
-
-    # Agregar los nuevos datos al DataFrame principal
-    data = pd.concat([data, new_data], ignore_index=True)
-
-    # Guardar el DataFrame en un archivo CSV
     data.to_csv('novedades.csv', index=False)
 
     st.write("Datos guardados en el archivo CSV")
