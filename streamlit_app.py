d7878f14e9c664bddd9f10bf33af5e77facafb98
Andresanegas <naslajosua@gmail.com>
2024-11-28 20:53:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
 st.title("🎈 Yes Bpo")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+# Campos del formulario
+fecha = st.date_input("Fecha")
+nombre = st.text_input("Nombre")
+novedad = st.selectbox("Novedad", ["Llegada Tarde", "Incapacidad"])
+detalles = st.text_area("Detalles")
+
+# Botón para enviar los datos (en este caso, simplemente imprimimos los datos)
+if st.button("Enviar"):
+    st.write("Datos ingresados:")
+    st.write(f"Fecha: {fecha}")
+    st.write(f"Nombre: {nombre}")
+    st.write(f"Novedad: {novedad}")
+    st.write(f"Detalles: {detalles}")
